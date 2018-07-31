b855644b1c6a0a2e29d6de78860475815bec61e1
Konstantinos Margaritis <Konstantinos.Margaritis@gr.ey.com>
2018-07-31 15:49:38 +0300

    Refactoring

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,27 +25,23 @@ class App extends Component {
 	this.setState({searchfield: event.target.value})	
 	}
 
-	render(){
-		const filteredRobots = this.state.robots.filter(robots =>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
-		if(this.state.robots.length === 0){
-			return <h1> Loading . . . </h1>
-		}else{
-		return (
-		<div className='tc'>
-		<h1 className= 'f1'>RoboFriends</h1>
-		<SearchBox searchChange={this.onSearchChange}/>
-		<Scroll>
-		<ErrorBoundry>
-	     <CardList robots={filteredRobots}/>
-	     </ErrorBoundry>
-	    </Scroll> 
-		</div>
-
-		);
-	}
-	}
+	 render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
 	
 
