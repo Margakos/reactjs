580d92c0ef644d6defdc79a8798e779b979642d5
Konstantinos Margaritis <Konstantinos.Margaritis@gr.ey.com>
2018-07-31 12:52:07 +0300

    1st com

---
+++ src/robots.js
@@ -0,0 +1,79 @@
+export const robots = [
+{
+	id:1,
+	name: 'Nick Kandy',
+	username: 'Kandy',
+	email: 'boukloman@me.com'
+},
+{
+	id:2,
+	name: 'Lambros Has',
+	username: 'Koliopas',
+	email: 'lamber@me.com'
+},
+{
+	id:3,
+	name: 'Babis Karbv',
+	username: 'Bread',
+	email: 'karv@me.com'
+},
+{
+	id:4,
+	name: 'Kostas Siac',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:5,
+	name: 'Kostas werewr',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:6,
+	name: 'Kostas Siqdsasdac',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:7,
+	name: 'Kostas asA',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:8,
+	name: 'Kostas AAAA',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:9,
+	name: 'Kostas SSSSiac',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+},
+{
+	id:10,
+	name: 'Kostas SiDSDSac',
+	username: 'Aisxos',
+	email: 'aisxos@me.com'
+}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
